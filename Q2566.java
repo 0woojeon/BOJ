0ac47d056c69ee6d9598086b072c7e86be7101c8
0woo22 <82591658+0woo22@users.noreply.github.com>
2025-02-09 16:54:00 +0900

    250209_Q2566

---
+++ Q2566.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Q2566 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;  
+        int row = 1;  
+        int col = 1;  
+
+        for (int i = 1; i <= 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j<= 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num > max) {
+                    max = num;
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " " + col);
+    }
+}
