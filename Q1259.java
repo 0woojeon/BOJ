86047528b3b6332ae0ad11c9d971d6260a450c5a
0woo22 <82591658+0woo22@users.noreply.github.com>
2025-02-14 21:32:44 +0900

    250214_Q1032

---
+++ Q1259.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Q1259 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		while(true) {
+			String S = br.readLine();
+			StringBuilder sb = new StringBuilder(S);
+			String rev = sb.reverse().toString();
+			if(S.equals("0")) break;
+			
+			if(S.equals(rev)) {
+				System.out.println("yes");
+			}else{
+				System.out.println("no");
+			}
+		}
+	}
+
+}
